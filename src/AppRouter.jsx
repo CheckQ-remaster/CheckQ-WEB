302f08dc2294f99d6bd9c3229d3f3e5ea3e58024
bjigeon <bjigeon@gmail.com>
2022-05-11 00:20:51 +0900

    fix error

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,30 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Redirect from "./components/common/Redirect";
+// import Redirect from "../src/components/common/Redirect";
 
-import NavBar from "./components/NavBar/NavBar";
-import Footer from "./components/Footer/Footer";
+// import NavBar from "./components/NavBar/NavBar";
+// import Footer from "./components/Footer/Footer";
+// import NotFound from "./NotFound";
 
-import Mainpage from "./components/Mainpage/Mainpage";
-import MyPage from "./components/MyPage/MyPage";
-import Problems from "./components/Problems/Problems";
-import Problem from "./components/Problems/Problem";
-import Exam from "./components/Exam/Exam";
-import QnA from "./components/QandA/QandA";
-import QnAPage from "./components/QandA/QnAPage";
-import Workbooks from "./components/Workbooks/Workbooks";
-import Groups from "./components/Group/Groups";
-import Memos from "./components/Memo/Memos.jsx";
+// import Loginpage from "./components/Authentication/Loginpage";
+// import Joinpage from "./components/Authentication/JoinPage";
+// import Register from "./components/Register/RegisterProblem";
+import ScrollToTop from "../src/components/common/ScrollToTop";
 
-import Loginpage from "./components/Authentication/Loginpage";
-import Joinpage from "./components/Authentication/JoinPage";
-
-import NotFound from "./NotFound";
-import Register from "./components/Register/RegisterProblem";
-import RegisterWorkbook from "./components/Register/RegisterWorkbook";
-import RegisterQuestion from "./components/Register/RegisterQuestion";
-import ScrollToTop from "./components/common/ScrollToTop";
-
-import userData from "./store/userData";
+import userData from "../src/components/common/userData";
 import { useRecoilState } from "recoil";
 import { useEffect } from "react";
 import axios from "axios";
@@ -42,18 +28,18 @@ const AppRouter = () => {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <NavBar />
+      {/* <NavBar /> */}
       <Routes>
         {getUserData === null ? (
           <>
-            <Route path="/login" element={<Loginpage />} />
+            {/* <Route path="/login" element={<Loginpage />} />
             <Route path="/join" element={<Joinpage />} />
-            {/* <Route path="*" element={<Redirect to={"/login"}/>}/> */}
+            <Route path="*" element={<Redirect to={"/login"} />} /> */}
           </>
         ) : (
           <>
-            <Route path="/" element={<Mainpage />} />
-            <Route path="/problems" element={<Problems />} />
+            {/* <Route path="/" element={<Mainpage />} /> */}
+            {/* <Route path="/problems" element={<Problems />} />
             <Route path="/problem/:number" element={<Problem />} />
             <Route path="/workbook" element={<Workbooks />} />
             <Route path="/exam" element={<Exam />} />
@@ -64,12 +50,12 @@ const AppRouter = () => {
             <Route path="/mypage" element={<MyPage />} />
             <Route path="/register" element={<Register />} />
             <Route path="/registerWorkbook" element={<RegisterWorkbook />} />
-            <Route path="/registerQuestion" element={<RegisterQuestion />} />
+            <Route path="/registerQuestion" element={<RegisterQuestion />} /> */}
           </>
         )}
-        <Route path="*" element={<NotFound />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 };
