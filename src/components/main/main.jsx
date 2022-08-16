7e94fc2bce2d7417bbe205dd97a5f652b69e275f
seohyeon1578 <seohyeon1578@naver.com>
2022-08-16 22:39:10 +0900

    main menu

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <h2>메인</h2>
+      <h2>메인 메뉴</h2>
+      <button onClick={() => navigate('/reservation')}>예약하기</button>
+      <button onClick={() => navigate('/myPage')}>나의 예약</button>
+      <button>Logout</button>
     </>
   );
 };
