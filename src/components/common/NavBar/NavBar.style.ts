b93d45a7c777b00a71dc9b7843f4409a0e4398e2
seohyeon1578 <seohyeon1578@naver.com>
2022-09-25 15:38:02 +0900

    change: navigation
    백그라운드 이미지가 보이지 않음.

--- src/components/common/NavBar/NavBar.style.ts
+++ src/components/common/NavBar/NavBar.style.ts
@@ -1,8 +1,56 @@
 import styled from "styled-components";
 
-export const Contaienr = styled.div`
-  width: 100%;
-  height: 100px;
+interface NavProps {
+  current: string
+  text: string
+}
 
-  background-color: gainsboro;
+export const Contaienr = styled.aside`
+  width: 100vw;
+  height: 80px;
+
+  background: #FFFFFF;
+  box-shadow: 0px -4px 20px rgba(0, 0, 0, 0.1);
+
+  display: flex;
+
+  justify-content: center;
+
+  position: fixed;
+  bottom: 0%;
+`;
+
+export const NavWrapper = styled.nav`
+  width: 375px;
+  height: 80px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const NavItem = styled.a<NavProps>`
+  width: 100px;
+  height: 100%;
+
+  text-decoration: none;
+  text-align: center;
+
+  cursor: pointer;
+
+  div {
+    background-image: url("../../../assets/image/Icon/${props => props.current === props.text ? 'focushome' : props.text}.png");
+    
+    &:hover {
+      background-image: url("../../../assets/image/Icon/${props => 'focus' + props.text}.png");
+    }
+  }
+
+  span {
+    color: ${props => props.current === props.text ? '#000000' : '#B4B4B4'};
+
+    &:hover {
+      color: #000000;
+    }
+  }
 `;
--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import * as N from "./NavBar.style";
 
 const NavBar = () => {
-  return <N.Contaienr>NAV</N.Contaienr>;
+  const location = useLocation();
+  const [currentPage, setCurrentPage] = useState(location.pathname.split('/')[1]);
+  
+  return (
+    <N.Contaienr>
+      <N.NavWrapper>
+        <N.NavItem current={currentPage} text={"home"} href="/home">
+          <div></div>
+          <span>홈</span>
+        </N.NavItem>
+        <N.NavItem current={currentPage} text={"reservation"} href="/reservation">
+          <div></div>
+          <span>예약</span>
+        </N.NavItem>
+        <N.NavItem current={currentPage} text={"my"} href="/">
+          <div></div>
+          <span>내 예약</span>
+        </N.NavItem>
+        <N.NavItem current={currentPage} text={"logout"} href="/">
+          <div></div>
+          <span>로그아웃</span>
+        </N.NavItem>
+      </N.NavWrapper>
+    </N.Contaienr>
+  );
 };
 
 export default NavBar;
